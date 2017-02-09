1aff501e8ef6e8bf288779ecf0fc52497ee0b9b5
Yong-il Lee <link@openable.net>
2017-02-09 09:18:05 +0900

    Cdot 클레스 추가

---
+++ ImplicitViewer/Model/Cdot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitViewer.Model
+{
+    class Cdot
+    {
+        public int x;
+        public int y;
+        public string word;
+
+        public Cdot()
+        {
+            x = -1;
+            y = -1;
+            word = "";
+        }
+
+        public Cdot(int x, int y, string w)
+        {
+            this.x = x;
+            this.y = y;
+            this.word = w;
+        }
+    }
+}
