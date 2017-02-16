d8d0d4e81ca9c6dbc644eea0d3361d947cc68fc5
Yong-il Lee <link@openable.net>
2017-02-16 18:25:49 +0900

    시선 raw data 인식 관련 맨 첫줄 뛰어넘어서 인식하던 것 바로 잡음

--- ImplicitViewer/Main.cs
+++ ImplicitViewer/Main.cs
@@ -265,12 +265,12 @@ namespace ImplicitViewer
                         }
                             
 
-                        for (int i = 0; i < (3 + 1); i++)
+                        for (int i = 0; i < 3; i++)
                             line = Setting.reader.ReadLine();
                     }
                     else
                     {
-                        for (int i = 0; i < (3 + 1 + 2 + 3 + 1); i++)
+                        for (int i = 0; i < (3 + 1 + 2 + 3); i++)
                             line = Setting.reader.ReadLine();
                     }
 
