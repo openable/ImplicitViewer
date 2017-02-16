8abf0c6eefbd440498ca6af6bed94709e42755a3
Yong-il Lee <link@openable.net>
2017-02-16 19:05:02 +0900

    Squre 클래스 추가

---
+++ ImplicitViewer/Model/Squre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitViewer.Model
+{
+    class Squre
+    {
+        public int x;   //x좌표
+        public int y;   //y좌표
+        public int w;   //너비 Width
+        public int h;   //높이 Height
+
+        public Squre()
+        {
+            x = -1;
+            y = -1;
+            w = -1;
+            h = -1;
+        }
+
+        public Squre(int x, int y, int w, int h)
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+    }
+}
