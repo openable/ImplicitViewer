be52fcac870ea909bc2eee5a9aeb22360cbd1875
Yong-il Lee <link@openable.net>
2017-02-18 13:11:03 +0900

    Main 클래스 결과 화면 버튼 클릭시 이미 로딩된 데이터가 있으면 파일 다시 읽지 않게 수정, 이래야 수정 시켰던 정보가 유지되고 있음

--- ImplicitViewer/Main.cs
+++ ImplicitViewer/Main.cs
@@ -73,7 +73,6 @@ namespace ImplicitViewer
 
         private void initReader()
         {
-
             string line;
             Setting.encode = System.Text.Encoding.GetEncoding("ks_c_5601-1987");
             if (fList == null)
@@ -158,219 +157,223 @@ namespace ImplicitViewer
 
         private void setTask()
         {
-            Setting.taskList = new ArrayList();
-            string line;
-            string[] w;
-            Setting.reader = new StreamReader(Setting.path, Setting.encode);
-
-            int type;
-            string stimulus;
-            Rect rStimuls;
-            int nChoice;
-            string[] choice;
-            Rect[] rChoice;
-
-            int pNum;        //문항번호
-            int rTime;       //응답시간
-            string dWord;    //선택단어
-            int sTime;       //자극단어 응시시간
-            int[] gTime;     //선택순서 응시시간 저장용
-
-            while((line = Setting.reader.ReadLine()) != null)
+            int? cnt = Setting.taskList?.Count;
+            if (cnt == null)
             {
-                if (line.IndexOf("문항번호:") != -1)
+                Setting.taskList = new ArrayList();
+                string line;
+                string[] w;
+                Setting.reader = new StreamReader(Setting.path, Setting.encode);
+
+                int type;
+                string stimulus;
+                Rect rStimuls;
+                int nChoice;
+                string[] choice;
+                Rect[] rChoice;
+
+                int pNum;        //문항번호
+                int rTime;       //응답시간
+                string dWord;    //선택단어
+                int sTime;       //자극단어 응시시간
+                int[] gTime;     //선택순서 응시시간 저장용
+
+                while ((line = Setting.reader.ReadLine()) != null)
                 {
-                    //문항번호 인식
-                    w = line.Split('\t');
-                    pNum = Int32.Parse(w[1]);
-
-                    //문항유형 인식
-                    line = Setting.reader.ReadLine();
-                    w = line.Split('\t');
-                    type = Int32.Parse(w[1]);
-
-                    //제시자극 인식
-                    line = Setting.reader.ReadLine();
-                    w = line.Split('\t');
-                    stimulus = w[1];
-
-                    rStimuls = new Rect();
-                    //제시자극이 2줄로 넘어오는 경우를 대비하기 위해 if문 예외처리
-                    if (w.Length == 6)
+                    if (line.IndexOf("문항번호:") != -1)
                     {
-                        rStimuls.x = (int)Convert.ToSingle(w[2]);
-                        rStimuls.y = (int)Convert.ToSingle(w[3]);
-                        // raw 데이터 출력에 이미 잘못되어서 Task 생성시 기본 환경 값을 토대로 바로 잡기로 함.
-                        rStimuls.w = (int)Convert.ToSingle(w[4]);
-                        rStimuls.h = (int)Convert.ToSingle(w[5]);
-                    }
+                        //문항번호 인식
+                        w = line.Split('\t');
+                        pNum = Int32.Parse(w[1]);
 
-                    //제시자극 인식 할 때 줄바꿈 들어간 경우 예외처리
-                    line = Setting.reader.ReadLine();
-                    w = line.Split('\t');
-                    if (!w[0].Equals("선택순서:"))
-                    {
-                        stimulus = stimulus + "\r\n" + w[0];
-                        rStimuls.x = (int)Convert.ToSingle(w[1]);
-                        rStimuls.y = (int)Convert.ToSingle(w[2]);
-                        // raw 데이터 출력에 이미 잘못되어서 Task 생성시 기본 환경 값을 토대로 바로 잡기로 함.
-                        rStimuls.w = (int)Convert.ToSingle(w[3]);
-                        rStimuls.h = (int)Convert.ToSingle(w[4]);
-                        line = Setting.reader.ReadLine();
-                    }
-
-                    //선택순서 인식
-                    if (type == 1)
-                        nChoice = 15;
-                    else
-                        nChoice = 2;
-                    
-                    choice = new string[nChoice];
-                    rChoice = new Rect[nChoice];
-                    for (int i = 0; i < nChoice; i++)
-                    {
+                        //문항유형 인식
                         line = Setting.reader.ReadLine();
                         w = line.Split('\t');
-                        choice[i] = w[0];
+                        type = Int32.Parse(w[1]);
 
-                        rChoice[i] = new Rect();
-                        if (w.Length < 6)
+                        //제시자극 인식
+                        line = Setting.reader.ReadLine();
+                        w = line.Split('\t');
+                        stimulus = w[1];
+
+                        rStimuls = new Rect();
+                        //제시자극이 2줄로 넘어오는 경우를 대비하기 위해 if문 예외처리
+                        if (w.Length == 6)
                         {
-                            rChoice[i].x = (int)Convert.ToSingle(w[1]);
-                            rChoice[i].y = (int)Convert.ToSingle(w[2]);
+                            rStimuls.x = (int)Convert.ToSingle(w[2]);
+                            rStimuls.y = (int)Convert.ToSingle(w[3]);
                             // raw 데이터 출력에 이미 잘못되어서 Task 생성시 기본 환경 값을 토대로 바로 잡기로 함.
-                            rChoice[i].w = (int)Convert.ToSingle(w[3]);
-                            rChoice[i].h = (int)Convert.ToSingle(w[4]);
+                            rStimuls.w = (int)Convert.ToSingle(w[4]);
+                            rStimuls.h = (int)Convert.ToSingle(w[5]);
+                        }
+
+                        //제시자극 인식 할 때 줄바꿈 들어간 경우 예외처리
+                        line = Setting.reader.ReadLine();
+                        w = line.Split('\t');
+                        if (!w[0].Equals("선택순서:"))
+                        {
+                            stimulus = stimulus + "\r\n" + w[0];
+                            rStimuls.x = (int)Convert.ToSingle(w[1]);
+                            rStimuls.y = (int)Convert.ToSingle(w[2]);
+                            // raw 데이터 출력에 이미 잘못되어서 Task 생성시 기본 환경 값을 토대로 바로 잡기로 함.
+                            rStimuls.w = (int)Convert.ToSingle(w[3]);
+                            rStimuls.h = (int)Convert.ToSingle(w[4]);
+                            line = Setting.reader.ReadLine();
+                        }
+
+                        //선택순서 인식
+                        if (type == 1)
+                            nChoice = 15;
+                        else
+                            nChoice = 2;
+
+                        choice = new string[nChoice];
+                        rChoice = new Rect[nChoice];
+                        for (int i = 0; i < nChoice; i++)
+                        {
+                            line = Setting.reader.ReadLine();
+                            w = line.Split('\t');
+                            choice[i] = w[0];
+
+                            rChoice[i] = new Rect();
+                            if (w.Length < 6)
+                            {
+                                rChoice[i].x = (int)Convert.ToSingle(w[1]);
+                                rChoice[i].y = (int)Convert.ToSingle(w[2]);
+                                // raw 데이터 출력에 이미 잘못되어서 Task 생성시 기본 환경 값을 토대로 바로 잡기로 함.
+                                rChoice[i].w = (int)Convert.ToSingle(w[3]);
+                                rChoice[i].h = (int)Convert.ToSingle(w[4]);
+                            }
+                            else
+                            {
+                                rChoice[i].x = (int)Convert.ToSingle(w[2]);
+                                rChoice[i].y = (int)Convert.ToSingle(w[3]);
+                                // raw 데이터 출력에 이미 잘못되어서 Task 생성시 기본 환경 값을 토대로 바로 잡기로 함.
+                                rChoice[i].w = (int)Convert.ToSingle(w[4]);
+                                rChoice[i].h = (int)Convert.ToSingle(w[5]);
+                            }
+                        }
+
+                        //문항유형과 제시자극, 선택순서 정보로 item 변수 생성
+                        Item item = new Item(type, stimulus);
+                        item.choice = choice;
+
+                        //제시자극과 선택순서 좌표 정보 설정
+                        item.rStimuls = rStimuls;
+                        item.rChoice = rChoice;
+
+                        //응답시간 인식
+                        line = Setting.reader.ReadLine();
+                        w = line.Split('\t');
+                        rTime = Int32.Parse(w[1]);
+
+                        //선택단어 인식
+                        line = Setting.reader.ReadLine();
+                        w = line.Split('\t');
+                        dWord = w[1];
+
+                        //응시시간 안내 줄 넘기기
+                        line = Setting.reader.ReadLine();
+
+                        //제시자극 응시시간 인식
+                        line = Setting.reader.ReadLine();
+                        w = line.Split('\t');
+                        sTime = (int)Convert.ToSingle(w[1]);
+
+                        //선택순서 응시시간 인식
+                        if (type == 1)
+                        {
+                            gTime = new int[15];
+                            for (int i = 0; i < 15; i++)
+                            {
+                                line = Setting.reader.ReadLine();
+                                w = line.Split('\t');
+                                gTime[i] = (int)Convert.ToSingle(w[1]);
+                            }
                         }
                         else
                         {
-                            rChoice[i].x = (int)Convert.ToSingle(w[2]);
-                            rChoice[i].y = (int)Convert.ToSingle(w[3]);
-                            // raw 데이터 출력에 이미 잘못되어서 Task 생성시 기본 환경 값을 토대로 바로 잡기로 함.
-                            rChoice[i].w = (int)Convert.ToSingle(w[4]);
-                            rChoice[i].h = (int)Convert.ToSingle(w[5]);
-                        }
-                    }
-
-                    //문항유형과 제시자극, 선택순서 정보로 item 변수 생성
-                    Item item = new Item(type, stimulus);
-                    item.choice = choice;
-
-                    //제시자극과 선택순서 좌표 정보 설정
-                    item.rStimuls = rStimuls;
-                    item.rChoice = rChoice;
-
-                    //응답시간 인식
-                    line = Setting.reader.ReadLine();
-                    w = line.Split('\t');
-                    rTime = Int32.Parse(w[1]);
-
-                    //선택단어 인식
-                    line = Setting.reader.ReadLine();
-                    w = line.Split('\t');
-                    dWord = w[1];
-
-                    //응시시간 안내 줄 넘기기
-                    line = Setting.reader.ReadLine();
-
-                    //제시자극 응시시간 인식
-                    line = Setting.reader.ReadLine();
-                    w = line.Split('\t');
-                    sTime = (int)Convert.ToSingle(w[1]);
-
-                    //선택순서 응시시간 인식
-                    if (type == 1)
-                    {
-                        gTime = new int[15];
-                        for (int i = 0; i < 15; i++)
-                        {
-                            line = Setting.reader.ReadLine();
-                            w = line.Split('\t');
-                            gTime[i] = (int)Convert.ToSingle(w[1]);
-                        }
-                    }
-                    else
-                    {
-                        gTime = new int[2];
-                        for (int i = 0; i < 2; i++)
-                        {
-                            line = Setting.reader.ReadLine();
-                            w = line.Split('\t');
-                            gTime[i] = (int)Convert.ToSingle(w[1]);
-                        }
-                    }
-
-                    //총 응시시간, 총 이탈시간, 총 이동비중 정보는 인식하지 않고 뛰어넘음
-                    for (int i = 0; i < 3; i++)
-                        line = Setting.reader.ReadLine();
-
-                    //문항번호, 응답시간, 선택단어, 제시자극 응시시간, 선택순서 응시시간 정보 추가
-                    item.pNum = pNum;
-                    item.rTime = rTime;
-                    item.dWord = dWord;
-                    item.sTime = sTime;
-                    item.gTime = gTime;
-
-                    //시선응시 raw data line 인식
-                    item.cList = new ArrayList();
-                    while (!(line = Setting.reader.ReadLine()).Equals(""))
-                    {
-                        //시선이탈 정보 뛰어넘기 예외 처리
-                        if (line.IndexOf("[") != -1)
-                            continue;
-
-                        w = line.Split('\t');
-                        Cdot cdot = new Cdot(Convert.ToDouble(w[0]), Int32.Parse(w[1]), Int32.Parse(w[2]));
-                        if (w.Length > 3)
-                            cdot.word = w[3];
-                        item.cList.Add(cdot);
-                    }
-
-                    Setting.taskList.Add(item);
-                }
-            }
-
-            Setting.reader.Close();
-
-            if (fList != null)
-            {
-                textBox1.Text = "1 / " + fList.Length;
-
-                for (int k = 1; k < fList.Length; k++)
-                {
-                    int num = 0;
-                    Setting.reader = new StreamReader(fList[k], Setting.encode);
-
-
-                    while ((line = Setting.reader.ReadLine()) != null)
-                    {
-                        if (line.IndexOf("총 이동비중:") != -1)
-                        {
-                            Item item = (Item)Setting.taskList[num];
-
-                            while (!(line = Setting.reader.ReadLine()).Equals(""))
+                            gTime = new int[2];
+                            for (int i = 0; i < 2; i++)
                             {
-                                if (line.IndexOf("[") != -1)
-                                    continue;
-
+                                line = Setting.reader.ReadLine();
                                 w = line.Split('\t');
-                                Cdot cdot = new Cdot(Int32.Parse(w[1]), Int32.Parse(w[2]));
-                                if (w.Length > 3)
-                                    cdot.word = w[3];
-                                item.cList.Add(cdot);
+                                gTime[i] = (int)Convert.ToSingle(w[1]);
                             }
-
-                            Setting.taskList[num] = item;
-                            num++;
                         }
+
+                        //총 응시시간, 총 이탈시간, 총 이동비중 정보는 인식하지 않고 뛰어넘음
+                        for (int i = 0; i < 3; i++)
+                            line = Setting.reader.ReadLine();
+
+                        //문항번호, 응답시간, 선택단어, 제시자극 응시시간, 선택순서 응시시간 정보 추가
+                        item.pNum = pNum;
+                        item.rTime = rTime;
+                        item.dWord = dWord;
+                        item.sTime = sTime;
+                        item.gTime = gTime;
+
+                        //시선응시 raw data line 인식
+                        item.cList = new ArrayList();
+                        while (!(line = Setting.reader.ReadLine()).Equals(""))
+                        {
+                            //시선이탈 정보 뛰어넘기 예외 처리
+                            if (line.IndexOf("[") != -1)
+                                continue;
+
+                            w = line.Split('\t');
+                            Cdot cdot = new Cdot(Convert.ToDouble(w[0]), Int32.Parse(w[1]), Int32.Parse(w[2]));
+                            if (w.Length > 3)
+                                cdot.word = w[3];
+                            item.cList.Add(cdot);
+                        }
+
+                        Setting.taskList.Add(item);
                     }
-
-
-                    Setting.reader.Close();
-                    textBox1.Text = (k + 1) + " / " + fList.Length;
                 }
-            }
 
+                Setting.reader.Close();
+
+                if (fList != null)
+                {
+                    textBox1.Text = "1 / " + fList.Length;
+
+                    for (int k = 1; k < fList.Length; k++)
+                    {
+                        int num = 0;
+                        Setting.reader = new StreamReader(fList[k], Setting.encode);
+
+
+                        while ((line = Setting.reader.ReadLine()) != null)
+                        {
+                            if (line.IndexOf("총 이동비중:") != -1)
+                            {
+                                Item item = (Item)Setting.taskList[num];
+
+                                while (!(line = Setting.reader.ReadLine()).Equals(""))
+                                {
+                                    if (line.IndexOf("[") != -1)
+                                        continue;
+
+                                    w = line.Split('\t');
+                                    Cdot cdot = new Cdot(Int32.Parse(w[1]), Int32.Parse(w[2]));
+                                    if (w.Length > 3)
+                                        cdot.word = w[3];
+                                    item.cList.Add(cdot);
+                                }
+
+                                Setting.taskList[num] = item;
+                                num++;
+                            }
+                        }
+
+
+                        Setting.reader.Close();
+                        textBox1.Text = (k + 1) + " / " + fList.Length;
+                    }
+                }
+
+            }
             current = 0;
             showTask();
         }
