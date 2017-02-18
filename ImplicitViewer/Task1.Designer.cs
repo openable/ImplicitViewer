9fb0f226c1da1134643a0baef604619f81edaed5
Yong-il Lee <link@openable.net>
2017-02-18 09:46:39 +0900

    Task1, Task2 클래스 '이동' 버튼 이름 '변경'으로 변경

--- ImplicitViewer/Task1.Designer.cs
+++ ImplicitViewer/Task1.Designer.cs
@@ -143,7 +143,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(40, 23);
             this.button5.TabIndex = 11;
-            this.button5.Text = "이동";
+            this.button5.Text = "변경";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
